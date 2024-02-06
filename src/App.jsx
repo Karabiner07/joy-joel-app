a45a95f025f8989a8183ac080f5ed8bd2b5c4513
Karabiner07 <joyphilip.p2001@gmail.com>
2024-02-07 03:13:03 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      
+      <h1>Cloud Computing</h1>
+      <h3>Complexity++;</h3>
+      <p>
+          Presentation by Joy and Joel
+        </p>
+      <div className="card">
+        
+      </div>
+      <p className="read-the-docs">
+        Rise Prakasam - MCA 1
+      </p>
+    </>
+  )
+}
+
+export default App
